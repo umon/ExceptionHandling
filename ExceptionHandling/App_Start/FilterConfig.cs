62dc146b7e645f063e530302324e66bd536b3ad3
umon <umutonur@outlook.com>
2019-11-17 11:18:23 +0300

    Örnek Proje oluşturuldu

---
+++ ExceptionHandling/App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using ExceptionHandling.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExceptionHandling.App_Start
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilter(GlobalFilterCollection filters)
+        {
+            filters.Add(new MyExceptionHandler());
+        }
+    }
+}
