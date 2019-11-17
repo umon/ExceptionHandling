bcb8f0dc419e314e54ed0d72e56e67d5c6f3db89
Umut ONUR <umutonur@outlook.com>
2019-11-17 11:36:33 +0300

    Örnek Proje oluşturuldu

---
+++ ExceptionHandling/Handlers/MyExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace ExceptionHandling.Handlers
+{
+    public class MyExceptionHandler : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            // Hatanın ilk yakalandığı yer
+        }
+    }
+}
---
+++ ExceptionHandling/Models/RegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExceptionHandling.Models
+{
+    public class RegisterModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
