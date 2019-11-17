5bcd78052ff35bcb9d9e17ea9a8df640d6ad5c42
Umut ONUR <umutonur@outlook.com>
2019-11-17 11:33:19 +0300

    Revert "Örnek Proje oluşturuldu"
    This reverts commit 62dc146b7e645f063e530302324e66bd536b3ad3.

--- ExceptionHandling/Models/RegisterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExceptionHandling.Models
-{
-    public class RegisterModel
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Name { get; set; }
-    }
-}
